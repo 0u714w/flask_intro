03c36014079bf617d29b68111132541dae811b61
Doug Enas <douglassenas@gmail.com>
2019-04-16 13:52:52 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+#! python3
+
+from flask import Flask, render_template
+from tinydb import TinyDB
+from random import randint
+
+
+app = Flask(__name__)
+db = TinyDB('db.json')
+
+@app.route('/')
+def print_recipe(recipe=None):
+    return render_template('recipe.html', recipe=random_recipe())
+
+def random_recipe():
+    all = db.all()
+    random = all[randint(0,1)]
+    return random
+
